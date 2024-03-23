bafe5bab665fdbcd7347e1e8f392e0a4fab7f43a
Amilkar Sanchez <amilkaralan@gmail.com>
2024-03-22 22:40:53 -0600

    form initialized

--- client/src/components/Form/FormCreation.jsx
+++ client/src/components/Form/FormCreation.jsx
@@ -23,29 +23,58 @@ export const FormCreation = ({ temperaments }) => {
 
 
   return (
-    <form className={style.form_wrapper}>
-      <label >Name: </label>
-      <input type="text" />
+    <form className={ style.form_wrapper }>
+      <label htmlFor='name'>Breed Name: </label>
+      <input name="name" type="text" />
+      <label htmlFor="years">What is the life expectancy of this breed?</label>
+      <input name='years' type="number" /> <span>to</span>
+      <input name='years' type="number" />
+      <label htmlFor="type_weitght">
+        What kind of measurement do you want to use for weight?
+      </label>
+      <select name="type_weight" id="weight_type">
+        <option value=""></option>
+        <option value="imperial">Lbs</option>
+        <option value="metric">Kg</option>
+      </select>
+      <label htmlFor="weight">
+        What weights does the breed fall into?
+      </label>
+      <input type="number" /> <span>to</span>
+      <input type="number" />
+      <label htmlFor="type_heitght">
+        What kind of measurement do you want to use for height?
+      </label>
+      <select name="type_height" id="height_type">
+        <option value=""></option>
+        <option value="imperial">Imperial</option>
+        <option value="metric">Metric</option>
+      </select>
+      <label htmlFor="height">
+        What is the height range of the breed?
+      </label>
+      <input type="number" /> <span>to</span>
+      <input type="number" />
       <div className={ style.form_temperament_wrapper }>
         <label onClick={ handleOpenOptions }>
-          Select some temperaments for your dog
+          Select a tag that describes the temperament of the breed
         </label>
-        <div className={tempOpen ? `${style.form_temperament_popup} ${style.open}` : style.form_temperament_popup}>
-        <ul className={ style.form_temperament_list_wrapper }>
-          { temperaments.map(temp => (
-            <li key={ temp.id } className={ style.form_temperament_list }>
-              <label>
-                <input
-                  type="checkbox"
-                  name="temperaments"
-                  value={ temp.name }
-                  onChange={ handleCheckboxChange }
-                />
-                { temp.name }
-              </label>
-            </li>
-          )) }
-        </ul>
+        <div className={ tempOpen ? `${style.form_temperament_popup} ${style.open}` : style.form_temperament_popup }>
+          <ul className={ style.form_temperament_list_wrapper }>
+            { temperaments.map(temp => (
+              <li key={ temp.id } className={ style.form_temperament_list }>
+                <label>
+                  <input
+                    type="checkbox"
+                    name="temperaments"
+                    value={ temp.name }
+                    onChange={ handleCheckboxChange }
+                  />
+                  { temp.name }
+                </label>
+              </li>
+            )) }
+          </ul>
         </div>
       </div>
     </form>
--- client/src/routes/Create/Create.jsx
+++ client/src/routes/Create/Create.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
 import { FormCreation } from '../../components/Form/FormCreation'
 import style from './Create.module.css'
 
 export const Create = ({temperaments}) => {
+  const [open, setOpen] = useState();
+
   return (
     <div className={style.create_main}>
+      <div className={open ? `${style.create_image_wrapper} ${style.open}` : style.create_image_wrapper} onClick={()=>setOpen(!open)}>
+        <img src="https://th.bing.com/th/id/OIG3.qGB_V63E.SksgZqHeIaI?pid=ImgGn" alt="" />
+      </div>
       <FormCreation temperaments={temperaments}/>
     </div>
   )
